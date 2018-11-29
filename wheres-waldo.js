b0537a03dc822ac22c504046aa51bbc3a01bbd19
Gioanna Rheumer <ratofkryll@gmail.com>
2018-11-29 19:33:23 +0000

    refactored to include for Each

--- wheres-waldo.js
+++ wheres-waldo.js
@@ -1,13 +1,14 @@
 // The second argument/parameter is expected to be a function
+var names = ["Alice", "Bob", "Waldo", "Winston"];
 var waldoIndex = 0;
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  names.forEach(function(name, i) {
+    if (name === "Waldo") {
       waldoIndex = i;
       found();   // execute callback
     }
-  }
+  });
   return waldoIndex;
 }
 
@@ -15,4 +16,4 @@ function actionWhenFound() {
   console.log(`Found Waldo at index ${waldoIndex}!`);
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(names, actionWhenFound);
